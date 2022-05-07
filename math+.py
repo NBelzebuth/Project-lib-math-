dbc07d2c7ccc9687fce72361a3bd5210a91ed1c7
__belzebuth__ <104933800+NBelzebuth@users.noreply.github.com>
2022-05-07 16:31:28 +0200

    Update math+.py

--- math+.py
+++ math+.py
@@ -3,32 +3,25 @@ def try_int(str: str, convert=False):
         return int(str)
     except:
         return False
-
 def is_number(str: str):
     try:
         str = int(str)
         return True
     except:
         return False
-
 def exponent(n, exponent=2, do_round=True):
     if do_round: return round(n ** exponent)
     else: return n ** exponent
-
 def square(n, do_round=True):
     if do_round: return round(n ** 2)
     else: return n ** 2
-
 def square_root(n:int, do_round=True):
     if do_round: return round(n ** (1/2))
     else: return n ** (1/2)
-
 def is_even(n:int):
     return round(n/2) == n/2
-
 def is_not_even(n:int):
     return not is_even(n)
-
 def is_prime(n):
     return_value = True
     n = int(n)
@@ -37,17 +30,13 @@ def is_prime(n):
             return_value = False
 
     return return_value
-
 def is_not_prime(n):
     return not is_prime(n)
-
 def pyth(l1, l2, do_round=True):
     return square_root(square(l1, do_round) + square(l2, do_round), do_round)
-
-def get_percentage(total=100, nb=100, do_round=False):
+def get_percentage(total=100, nb=100, do_round=True):
     if do_round: return round((100*nb)/total)
     else: return (100*nb)/total
-
-def percentage(percentage=100, total=100, do_round=False):
-    if do_round: return round()
+def percentage(percentage=100, total=100, do_round=True):
+    if do_round: return round((percentage*total)/100)
     else: return (percentage*total)/100
